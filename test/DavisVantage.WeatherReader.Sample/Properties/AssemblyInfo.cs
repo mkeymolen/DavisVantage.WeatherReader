6307b74b90bfc5042c57ed6f5e0fc47fa5ddac5c
mkeymolen <keymolen.michael@gmail.com>
2016-12-22 23:11:54 +0100

    Added sample project

---
+++ test/DavisVantage.WeatherReader.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DavisVantage.WeatherReader.Sample")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aaa64fa2-da2e-4d34-8fa7-3bb18f256c13")]
