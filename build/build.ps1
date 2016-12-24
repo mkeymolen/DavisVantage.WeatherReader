422151c54f1eb91bd5622c663440982bbd420d6d
mkeymolen <keymolen.michael@gmail.com>
2016-12-24 17:32:27 +0100

    build fix

---
+++ build/build.ps1
@@ -0,0 +1,10 @@
+param (
+	[string]$BuildVersionNumber=$(throw "-BuildVersionNumber is required.")
+)
+Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object{ 
+    $ProjectJsonPath =  $_.FullName
+    (gc -Path $ProjectJsonPath) `
+	    -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$BuildVersionNumber" |
+	        sc -Path $ProjectJsonPath -Encoding UTF8
+}
+dotnet build "src\DavisVantage.WeatherReader" --version-suffix $BuildVersionNumber
