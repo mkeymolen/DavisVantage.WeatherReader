7e8ac071569fe3da92b963d85233163ef02c1b48
mkeymolen <keymolen.michael@gmail.com>
2016-12-25 12:39:03 +0100

    nuget setup added

--- build/build.ps1
+++ build/build.ps1
@@ -1,10 +1,18 @@
 param (
-	[string]$BuildVersionNumber=$(throw "-BuildVersionNumber is required.")
+	[string]$BuildVersionNumber=$(throw "-BuildVersionNumber is required."),
+	[string]$TagVersionNumber
 )
 Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object{ 
     $ProjectJsonPath =  $_.FullName
-    (gc -Path $ProjectJsonPath) `
-	    -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$BuildVersionNumber" |
-	        sc -Path $ProjectJsonPath -Encoding UTF8
+    if ($TagVersionNumber){
+        (gc -Path $ProjectJsonPath) `
+	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$TagVersionNumber" |
+	            sc -Path $ProjectJsonPath -Encoding UTF8
+    }
+    else{
+        (gc -Path $ProjectJsonPath) `
+	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$BuildVersionNumber" |
+	            sc -Path $ProjectJsonPath -Encoding UTF8
+    }
 }
-dotnet pack "src\DavisVantage.WeatherReader" --version-suffix $BuildVersionNumber
+dotnet pack "src\DavisVantage.WeatherReader"
