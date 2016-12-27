4e7a8ddc79f163cdca1cc7cbfaf70fedc9649171
mkeymolen <keymolen.michael@gmail.com>
2016-12-27 13:15:05 +0100

    Comments added

--- src/DavisVantage.WeatherReader/IDataLogger.cs
+++ src/DavisVantage.WeatherReader/IDataLogger.cs
@@ -5,9 +5,23 @@ namespace DavisVantage.WeatherReader
 {
     public interface IDataLogger<in T> where T : IDataLoggerSettings
     {
+        /// <summary>
+        /// Connect to Console
+        /// </summary>
+        /// <param name="settings">dataloggersettings of type IDataLoggerSettings</param>
+        /// <returns>Connection succeeded or not</returns>
         bool Connect(T settings);
+        /// <summary>
+        /// WakeUp console. Not necessary to call this method seperately
+        /// </summary>
         void WakeUp();
+        /// <summary>
+        /// Read current weather data from console
+        /// </summary>
+        /// <param name="valueInMetric">Specify if returned values should be converted to metric or to keep imperial</param>
+        /// <returns>CurrentWeather object with ConsoleInfo included</returns>
         CurrentWeather ReadCurrentWeather(bool valueInMetric);
-        void ReadDayExtremes(bool valueInMetric);
+
+        void ReadExtremes(bool valueInMetric);
     }
 }
