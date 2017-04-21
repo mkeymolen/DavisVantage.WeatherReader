efbecac4a0d2fcb91713bfa6049bd47f732807d7
mkeymolen <keymolen.michael@gmail.com>
2017-04-21 23:01:15 +0200

    fix for appveyor file

--- build/build.ps1
+++ build/build.ps1
@@ -9,18 +9,18 @@ if ($TagVersionNumber){
 else{
 	Write-Host "Version: $BuildVersionNumber"
 }
-Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object{ 
+
+Get-ChildItem -Path $PSScriptRoot\..\src -Filter *.csproj -Recurse | ForEach-Object{ 
     $ProjectJsonPath =  $_.FullName
+	$csproj = [xml](Get-Content $ProjectJsonPath)
     if ($TagVersionNumber){
-        (gc -Path $ProjectJsonPath) `
-	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$TagVersionNumber" |
-	            sc -Path $ProjectJsonPath -Encoding UTF8
+       $csproj.Project.PropertyGroup.VersionPrefix = $TagVersionNumber
+	   $csproj.Save($ProjectJsonPath)
     }
     else{
-        (gc -Path $ProjectJsonPath) `
-	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$BuildVersionNumber" |
-	            sc -Path $ProjectJsonPath -Encoding UTF8
+       $csproj.Project.PropertyGroup.VersionPrefix = $BuildVersionNumber
+	   $csproj.Save($ProjectJsonPath)
     }
 }
-dotnet pack "$PSScriptRoot\..\src\DavisVantage.WeatherReader\project.json"
-dotnet build "$PSScriptRoot\..\sample\DavisVantage.WeatherReader.Sample"
+dotnet pack "$PSScriptRoot\..\src\DavisVantage.WeatherReader\DavisVantage.WeatherReader.csproj"
+dotnet build "$PSScriptRoot\..\samples\Sample.NetCore.Autofac\Sample.NetCore.Autofac.csproj"
